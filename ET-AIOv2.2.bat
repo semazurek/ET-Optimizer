48d568c24deb7b53661b811e864bac35492617de
Sebastian <85984736+MrRikey@users.noreply.github.com>
2022-02-26 19:56:49 +0100

    Update ET-AIOv2.2.bat

--- ET-AIOv2.2.bat
+++ ET-AIOv2.2.bat
@@ -96,7 +96,7 @@ REM 0 = hide completely, 1 = show only icon, 2 = show long search box
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 1 /f
 
 REM Disable News and Interests on Taskbar
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds /v EnableFeeds /t REG_DOWRD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" /v EnableFeeds /t REG_DWORD /d 0 /f
 
 REM  Disable MRU lists (jump lists) of XAML apps in Start Menu 
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_TrackDocs" /t REG_DWORD /d 0 /f
